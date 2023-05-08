b36639a4e7b2342eb430b282bb7bfbb5476c91ee
lingsat <lingsatpilingast@gmail.com>
2023-05-08 12:57:45 +0300

    feat: create redux store

---
+++ src/features/products/productsSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store/store";
+import { IProduct } from "./types/product.interface";
+
+interface ProductsState {
+  products: IProduct[];
+}
+
+const initialState: ProductsState = {
+  products: [],
+};
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+});
+
+export const selectProducts = (state: RootState) => state.products.products;
+
+export default productsSlice.reducer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
