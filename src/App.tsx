f67ff84f66ae14e3d6c7a9fb67c5f80ae28cd103
lingsat <lingsatpilingast@gmail.com>
2023-05-08 17:24:42 +0300

    ref: add craco paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import ProductsListPage from "@/features/products/pages/productsListPage/ProductsListPage";
-import ProductItemPage from "@/features/products/pages/productItemPage/ProductItemPage";
-import CartPage from "@/features/products/pages/CartPage/CartPage";
-import "./App.scss";
+import ProductsListPage from "@features/products/pages/productsListPage/ProductsListPage";
+import ProductItemPage from "@features/products/pages/productItemPage/ProductItemPage";
+import CartPage from "@features/products/pages/CartPage/CartPage";
+import "@/App.scss";
 
 const App = () => {
   return (
