cb793da8c10ee5af89c40d2e56de4fde13e6712d
lingsat <lingsatpilingast@gmail.com>
2023-05-10 15:57:10 +0300

    feat: add mobile styles for products list

--- src/features/products/components/ListFilter/ListFilter.tsx
+++ src/features/products/components/ListFilter/ListFilter.tsx
@@ -1,8 +1,39 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import arrowDownThin from "@/assets/images/arrow_down_thin.svg";
 import "./ListFilter.scss";
 
 const ListFilter: FC = () => {
-  return <div className="list-filter">Filter</div>;
+  const [showFilter, setShowFIlter] = useState<boolean>(false);
+
+  const handleFilterHide = () => {
+    setShowFIlter(false);
+  };
+
+  const handleFilterShow = () => {
+    setShowFIlter(true);
+  };
+
+  return (
+    <>
+      <div
+        className={`filter ${showFilter && "filter--show"}`}
+        onMouseEnter={handleFilterShow}
+        onMouseLeave={handleFilterHide}>
+        Filter
+      </div>
+      <div
+        className="filter__switcher"
+        onMouseEnter={handleFilterShow}
+        onMouseLeave={handleFilterHide}>
+        Show Filter
+        <img
+          className={`${showFilter && "reverse__icon"}`}
+          src={arrowDownThin}
+          alt="DownArrow"
+        />
+      </div>
+    </>
+  );
 };
 
 export default ListFilter;
