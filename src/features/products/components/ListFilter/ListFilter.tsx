8e58a30507cb8ee939ac4e71823a4672aac6a0ad
lingsat <lingsatpilingast@gmail.com>
2023-05-12 16:52:50 +0300

    feat: add price slider

--- src/features/products/components/ListFilter/ListFilter.tsx
+++ src/features/products/components/ListFilter/ListFilter.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from "react";
-import arrowDownThin from "@/assets/images/arrow_down_thin.svg";
+import React, { ChangeEvent, FC, useState } from "react";
+import ReactSlider from "react-slider";
 import { ICategoryWithCount } from "@products/types/caregory.interface";
+import arrowDownThin from "@/assets/images/arrow_down_thin.svg";
 import fiveStars from "@/assets/images/stars_five.svg";
 import fourStars from "@/assets/images/stars_four.svg";
 import threeStars from "@/assets/images/stars_three.svg";
@@ -15,6 +16,16 @@ interface ListFilterProps {
 
 const ListFilter: FC<ListFilterProps> = ({ categories, brands }) => {
   const [showFilter, setShowFIlter] = useState<boolean>(false);
+
+  const [priceValues, setPriceValues] = useState<number[]>([0, 500]);
+
+  const changeMinPrice = (event: ChangeEvent<HTMLInputElement>) => {
+    setPriceValues((prevValues) => [+event.target.value, prevValues[1]]);
+  };
+
+  const changeMaxPrice = (event: ChangeEvent<HTMLInputElement>) => {
+    setPriceValues((prevValues) => [prevValues[0], +event.target.value]);
+  };
 
   const handleFilterHide = () => {
     setShowFIlter(false);
@@ -85,15 +96,25 @@ const ListFilter: FC<ListFilterProps> = ({ categories, brands }) => {
         </div>
         <div className="filter__block">
           <h3 className="filter__title">Price</h3>
+          <ReactSlider
+            className="price-slider"
+            thumbClassName="price-slider__thumb"
+            trackClassName="price-slider__track"
+            value={priceValues}
+            onChange={setPriceValues}
+            min={0}
+            max={500}
+            pearling
+            minDistance={5}
+          />
           <form className="filter-price">
             <label className="filter-price__label">
               Min
               <input
                 className="filter-price__input"
                 type="number"
-                value={0}
-                min={0}
-                max={10000}
+                value={priceValues[0]}
+                onChange={changeMinPrice}
               />
             </label>
             <label className="filter-price__label">
@@ -101,9 +122,8 @@ const ListFilter: FC<ListFilterProps> = ({ categories, brands }) => {
               <input
                 className="filter-price__input"
                 type="number"
-                value={10000}
-                min={0}
-                max={10000}
+                value={priceValues[1]}
+                onChange={changeMaxPrice}
               />
             </label>
           </form>
