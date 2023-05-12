3cf97f0456fea7a3781b08fdce8042b505abfeb3
lingsat <lingsatpilingast@gmail.com>
2023-05-12 10:52:18 +0300

    feat: add categories and brands ui to aside filter

--- src/features/products/components/ListFilter/ListFilter.tsx
+++ src/features/products/components/ListFilter/ListFilter.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from "react";
 import arrowDownThin from "@/assets/images/arrow_down_thin.svg";
+import { categories } from "@/mock/categories";
+import { brands } from "@/mock/brands";
 import "./ListFilter.scss";
 
 const ListFilter: FC = () => {
@@ -19,7 +21,41 @@ const ListFilter: FC = () => {
         className={`filter ${showFilter && "filter--show"}`}
         onMouseEnter={handleFilterShow}
         onMouseLeave={handleFilterHide}>
-        Filter
+        <div className="filter__block">
+          <h3 className="filter__title">Categories</h3>
+          <ul className="filter-categories">
+            {categories.map((category, index) => (
+              <li
+                key={`filterCat-${category}-${index}`}
+                className="filter-categories__item">
+                <button className="filter-categories__btn">
+                  <p>{category}</p>
+                  <span>125</span>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="filter__block">
+          <h3 className="filter__title">Brands</h3>
+          <form className="filter-brands">
+            {brands.map((brand, index) => (
+              <fieldset
+                key={`filterBrand-${brand}-${index}`}
+                className="filter-brands__item">
+                <input
+                  className="filter-brands__input"
+                  type="checkbox"
+                  name={brand}
+                  id={brand}
+                />
+                <label className="filter-brands__label" htmlFor={brand}>
+                  {brand}
+                </label>
+              </fieldset>
+            ))}
+          </form>
+        </div>
       </div>
       <div
         className="filter__switcher"
